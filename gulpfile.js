064289c3dcbb8e8bd1af7978b68d82fe0973a7e6
shonenada <shonenada@gmail.com>
2016-05-02 14:34:26 +0000

    Update config files

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ var styles = {
 };
 
 var html = {
-  src: '{' + project.src + ',' + project.src + '/views}',
+  src: '{' + project.src + ',' + project.src + '/templates}',
   exts: ['html', 'htm'],
   dist: project.dist,
 };
@@ -61,12 +61,20 @@ gulp.task('build', ['collect', 'style', 'webpack']);
 
 gulp.task('clean', ['clean:dist']);
 
+gulp.task('watch', ['build'], function() {
+  gulp.start('browser-sync');
+  gulp.watch(assets.glob(styles), ['style']);
+  gulp.watch(assets.glob(scripts), ['webpack']);
+  gulp.watch(images.src + '/**/*.{' + images.exts.join(',') + '}', ['collect:images', 'webpack']);
+  gulp.watch(html.src + '/**/*.{' + html.exts.join(',') + '}', ['collect:html', 'webpack']);
+});
+
 gulp.task('webpack', function() {
   gulp.src(assets.glob(scripts))
       .pipe(plumber())
       .pipe(named(function(file) {
         var dirname = path.basename(path.dirname(file.path));
-        var filename = paht.basename(file.path, path.extname(file.path));
+        var filename = path.basename(file.path, path.extname(file.path));
         path.join(dirname, filename);
       }))
       .pipe(webpack(project.webpack))
@@ -84,7 +92,7 @@ gulp.task('style', function() {
       .pipe(plumber())
       .pipe(stylus(options))
       .pipe(gulp.dest(styles.dist))
-      .pipe(browserSync.reload({stream: ture}))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('collect', ['collect:image', 'collect:html']);
@@ -109,5 +117,5 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('clean:dist', function(done) {
-  del [project.dist], done
+  del([project.dist], done);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 var webpack = require('webpack');
 var nib = require('nib');
 var argv = require('yargs').argv;
+var path = require('path');
 
 var plugins = [
   new webpack.optimize.CommonsChunkPlugin('common.js'),
   !argv.debug ? new webpack.optimize.UglifyJsPlugin() : void 0,
-]
+];
 
 module.exports = {
+  entry: './src/scripts/Howler',
+  output: { filename: 'bundle.js' },
   resolve: {
+    root: path.resolve('./src'),
     extensions: ['', '.webpack.js', '.js', '.jsx', '.styl'],
     moduleDirectory: [
-      'node_modules', 'src/scripts', 'src/styles'
+      'node_modules', 'src/scripts', 'src/views', 'src/styles'
     ],
   },
-  output: {
-    filename: 'bundle.js',
-  },
   module: {
-    loader: [
+    loaders: [
       { test: /\.html$/, loader: 'raw'},
-      { test: /\.jsx$/, loader: 'babel', exclude: /(node_modules)/, query: { presets: ['es2015'] } },
+      { test: /\.jsx?$/, loader: 'babel', exclude: /(node_modules)/, query: { presets: ['es2015'] } },
       { test: /\.css$/, loader: 'style!css'},
       { test: /\.styl$/, loader: 'style!css!stylus' },
     ]
